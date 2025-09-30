5b3dea2861ab656aea26e960ce212a3b0d2df770
AdrielLuniere <adrielbinda@gmail.com>
2025-09-30 00:44:41 -0300

    Organizando os components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b">
+            <h1>My Website</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
